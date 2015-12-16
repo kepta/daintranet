397e7b5ef4649eecdcd678b98f3ccab097e0cd33
Kushan Joshi <0o3ko0@gmail.com>
2015-12-16 19:48:30 +0530

    render file

--- app/components/Intranet/Folder.js
+++ app/components/Intranet/Folder.js
@@ -10,11 +10,13 @@ export default class Viewer extends Base {
       };
     }
     getLocation() {
-        
+
     }
     render() {
       return (
-
+        <div>helolo
+          <a href="http://128.199.173.123:3000/api/intranet/23?loc=/Lecture/8-86.pdf">click here</a>
+        </div>
       );
     }
 }
--- app/network/Fetch.js
+++ app/network/Fetch.js
@@ -3,7 +3,7 @@ import { MailParser } from 'mailparser';
 const local = window.location.href.indexOf('localhost');
 
 // export const BASEURL = 'http://localhost:1337/webmail.daiict.ac.in';
-export const BASEURL = local === -1 ? 'http://128.199.173.123:3000/api'
+export const BASEURL = local !== -1 ? 'http://128.199.173.123:3000/api'
                                       : 'http://localhost:3000/api';
 
 const TIMER = 7000;
